dd4231688586e1202270579cab1680f3be166b37
Arie <arieardinatakusuma3@gmail.com>
2026-09-15 12:53:32 +0700

    big reformation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const words = [
   "IT Student",
   "Web Programming Enthusiast",
-  "Cybersecurity Enthusiast",
+  "Cybersecurity Enthusiast ",
 ];
 
 let wordIndex = 0;
@@ -35,3 +35,21 @@ function typeEffect() {
 }
 
 document.addEventListener("DOMContentLoaded", typeEffect);
+
+// Slider Auto-Fade Foto Profil
+const images = document.querySelectorAll(".hero-img");
+let currentImgIndex = 0;
+
+function changeImage() {
+  // Sembunyikan foto saat ini (hapus class active)
+  images[currentImgIndex].classList.remove("active");
+
+  // Pindah ke indeks foto berikutnya
+  currentImgIndex = (currentImgIndex + 1) % images.length;
+
+  // Tampilkan foto baru (tambah class active)
+  images[currentImgIndex].classList.add("active");
+}
+
+// Ganti foto setiap 4 detik (4000 milidetik)
+setInterval(changeImage, 4000);
